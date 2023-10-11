b62e00565f8f477d624de689e7bd3e9e63a76d9d
BintangDanuarta <60837533+BintangDanuarta@users.noreply.github.com>
2023-10-11 22:28:38 +0700

    Add files via upload

---
+++ MahasiswaBeraksi.java
@@ -0,0 +1,13 @@
+package Tugas;
+
+public class MahasiswaBeraksi {
+
+	public static void main(String[] args) {
+		// Bintang Danuarta (2210631170014)
+		Mahasiswa MHS = new Mahasiswa();
+		MHS.membaca();
+		MHS.nyontek();
+		MHS.modif();
+	}
+
+}
