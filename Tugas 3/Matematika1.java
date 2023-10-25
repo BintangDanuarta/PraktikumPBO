6bab03a3c184ca56e8e637df11f9fa282ddc6fa2
BintangDanuarta <60837533+BintangDanuarta@users.noreply.github.com>
2023-10-25 22:56:12 +0700

    Add files via upload

---
+++ Tugas 3/Matematika1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas2;
+
+/**
+ *
+ * @author Bintang Danuarta
+ */
+public class Matematika1 {
+  public int pertambahan(int a, int b){
+    return a + b; }
+   public int pengurangan(int a, int b){
+    return a - b; }
+   public int perkalian(int a, int b){
+    return a * b; }
+   public double pembagian(int a, int b){
+    return a / b; }
+}  
