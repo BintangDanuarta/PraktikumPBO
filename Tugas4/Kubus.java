a528ea53bf22ec6b06023738ddbac73ca25ea284
BintangDanuarta <60837533+BintangDanuarta@users.noreply.github.com>
2023-11-01 23:06:55 +0700

    Add files via upload

---
+++ Tugas4/Kubus.java
@@ -0,0 +1,17 @@
+package tugas4;
+
+/**
+ *
+ * @author Bintang Danuarta
+ */
+public class Kubus {
+
+    Kubus(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    Kubus(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ Tugas4/Perhitungan.java
@@ -0,0 +1,14 @@
+package tugas4;
+
+/**
+ *
+ * @author Bintang Danuarta
+ */
+public class Perhitungan {
+    public double Volume() {
+        return 0;
+    }
+    public double LuasPermukaan() {
+        return 0;
+    }
+}
